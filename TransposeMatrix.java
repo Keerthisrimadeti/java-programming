acecc495bbf5f1046472c4408c058d699485a0d1
Keerthisrimadeti <153889460+Keerthisrimadeti@users.noreply.github.com>
2026-03-15 16:19:24 +0530

    Add files via upload

---
+++ TransposeMatrix.java
@@ -0,0 +1,11 @@
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        int n = 5;
+        int result = 1;
+        for(int i=1;i<=n;i++) {
+            result *= i;
+        }
+        System.out.println("Example program: TransposeMatrix");
+        System.out.println("Result value: " + result);
+    }
+}
