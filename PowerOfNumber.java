1bcc9c9effd66ce668c1cb660f21e7429963a048
Keerthisrimadeti <153889460+Keerthisrimadeti@users.noreply.github.com>
2026-03-15 16:17:43 +0530

    Add files via upload

---
+++ PowerOfNumber.java
@@ -0,0 +1,11 @@
+public class PowerOfNumber {
+    public static void main(String[] args) {
+        int n = 5;
+        int result = 1;
+        for(int i=1;i<=n;i++) {
+            result *= i;
+        }
+        System.out.println("Example program: PowerOfNumber");
+        System.out.println("Result value: " + result);
+    }
+}
