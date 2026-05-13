b653c2dcac8eac50fdd8c418f897e2d432db87e0
Keerthisrimadeti <153889460+Keerthisrimadeti@users.noreply.github.com>
2026-05-13 11:58:19 +0530

    Update NumberPattern1.java

--- NumberPattern1.java
+++ NumberPattern1.java
@@ -1,11 +1,15 @@
 public class NumberPattern1 {
     public static void main(String[] args) {
+
         int n = 5;
-        int result = 1;
-        for(int i=1;i<=n;i++) {
-            result *= i;
+
+        for(int i = 1; i <= n; i++) {
+
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
         }
-        System.out.println("Example program: NumberPattern1");
-        System.out.println("Result value: " + result);
     }
 }
