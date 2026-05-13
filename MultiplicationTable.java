8105da40cea3cdf2d23944276d812f1dfb005613
Keerthisrimadeti <153889460+Keerthisrimadeti@users.noreply.github.com>
2026-05-13 11:56:53 +0530

    Update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,11 +1,10 @@
 public class MultiplicationTable {
     public static void main(String[] args) {
+
         int n = 5;
-        int result = 1;
-        for(int i=1;i<=n;i++) {
-            result *= i;
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
         }
-        System.out.println("Example program: MultiplicationTable");
-        System.out.println("Result value: " + result);
     }
 }
